c478e4c6ca5b4c82a41a461c9286882155a30680
Hedayat Farahi <hedayatfarahi8@gmail.com>
2024-10-14 03:20:08 +0200

    remove the localhost

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -93,8 +93,8 @@ export const createCheckoutSession = async ({
 
   // 8. Create a Stripe checkout session
   const stripeSession = await stripe.checkout.sessions.create({
-    success_url: `http://localhost:3000/thank-you?orderId=${order.id}`,
-    cancel_url: `http://localhost:3000/configure/preview?id=${configuration.id}`,
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
     payment_method_types: ["card", "paypal", "klarna", "sofort"],
     mode: "payment",
     shipping_address_collection: { allowed_countries: ["DE", "US"] },
